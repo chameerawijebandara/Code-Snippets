8d5837d428ba2ca452486e6fc54e7581f2f621b7
Chameera <chameera@liverooom.xyz>
2018-10-14 18:51:25 +0530

    Added util files
    Added print array function

---
+++ src/Util/Util.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+/**
+ *
+ * @author Chameera
+ */
+public class Util {
+
+    public static void printArray(int[] in) {
+        for (int a : in) {
+            System.out.print(a + " ");
+        }
+        System.out.println("");
+    }
+}
